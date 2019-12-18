1175e96699ef153413680f046f1fcb72eed46c3d
Tyler Jackson <tylerj3@gmail.com>
2019-12-18 13:54:56 -0500

    Getting Ready to enable recyclerView

--- app/src/main/java/com/example/finalprojectremake/data/BaseRepo.kt
+++ app/src/main/java/com/example/finalprojectremake/data/BaseRepo.kt
@@ -7,7 +7,7 @@ import java.io.IOException
 
 open class BaseRepo {
     suspend fun <T : Any> safeApiCall(call : suspend()-> Response<T>, error : String) :  T?{
-        val result = newsApiOutput(call, error)
+        val result = cardApiOutput(call, error)
         var output : T? = null
         when(result){
             is Output.Success ->
@@ -17,7 +17,7 @@ open class BaseRepo {
         return output
 
     }
-    private suspend fun<T : Any> newsApiOutput(call: suspend()-> Response<T> , error: String) : Output<T>{
+    private suspend fun<T : Any> cardApiOutput(call: suspend()-> Response<T> , error: String) : Output<T>{
         val response = call.invoke()
         return if (response.isSuccessful)
             Output.Success(response.body()!!)
