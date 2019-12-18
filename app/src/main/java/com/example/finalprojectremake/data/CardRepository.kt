64803c107b6f8b7611c3f6c12e1d5f7cff8cec68
Tyler Jackson <tylerj3@gmail.com>
2019-12-18 12:13:45 -0500

    AddingAuthToken

--- app/src/main/java/com/example/finalprojectremake/data/CardRepository.kt
+++ app/src/main/java/com/example/finalprojectremake/data/CardRepository.kt
@@ -1,12 +1,13 @@
 package com.example.finalprojectremake.data
 
 import com.example.finalprojectremake.api.BlizzardApiInterface
+import com.example.finalprojectremake.api.BlizzardAuthApiService.getAuthToken
 
 public class CardRepository(private val apiInterface: BlizzardApiInterface) : BaseRepository() {
     suspend fun getCardData(): MutableList<Card>? {
         return safeApiCall(
             //await the result of deferred type
-            call = { apiInterface.fetchCardDataAsync("").await() },
+            call = { apiInterface.fetchCardDataAsync(getAuthToken.toString()).await() },
             error = "Error Fetching Card Data"
         //convert to mutable list
         )?.cards?.toMutableList()
